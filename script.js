806ee045a69eb67b8602e40ac0af64caf246fde4
Max Meischke <meischkem@outlook.com>
2022-04-13 11:51:51 -0400

    Fixed whitespace Added comments for readability

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Player factory function
 const Player = (name, mark, turn) => {
     this.name = name;
     this.mark = mark;
@@ -6,33 +7,31 @@ const Player = (name, mark, turn) => {
     return {name, mark, turn, cellsMarked};
 };
 
-
 // Gameboard object holds gameboard in arr and handles the gameflow
 const gameboard = (() => {
     const boardCells = document.querySelectorAll('.grid-cell');
     let board = [];
+    // Create two players for the game
     const pl1 = Player('Player 1', 'X', true);
     const pl2 = Player('Player 2', 'O', false);
-
+    // Add click event for each cell
     boardCells.forEach(cell => {
         cell.addEventListener('click', e => {
             let cell =e.target;
             if(pl1.turn == true){
-                cell.classList.add('x-mark');
-                pl1.cellsMarked.push(e.target.id);
-                pl1.turn = false;
+                cell.classList.add('x-mark');  // add class to div to add mark
+                pl1.cellsMarked.push(e.target.id);  // add the cell "number" to cellsMarked arr
+                pl1.turn = false;  // Switch players turn
                 pl2.turn = true;
             }
             else if(pl2.turn == true){
-                cell.classList.add('o-mark');
-                pl2.cellsMarked.push(e.target.id);
-                pl1.turn = true;
+                cell.classList.add('o-mark');  // add class to div to add mark
+                pl2.cellsMarked.push(e.target.id);  // add the cell "number" to cellsMarked arr
+                pl1.turn = true;  // Switch players turn
                 pl2.turn = false;
             }
-
-        })
-    });
-        
+        });
+    });      
 })();
 
 
